15a7fb8a5aecfd6ba8d169ef278d483ea3a6a268
Snakevil Zen <zsnakevil@gmail.com>
2019-03-27 00:06:57 +0800

    填充书籍章节页。

--- share/gui/js/react/index.tsx
+++ share/gui/js/react/index.tsx
@@ -11,24 +11,22 @@ export default function (props: { [prop: string]: any }) {
         [
             target,
             setTarget
-        ] = React.useState(raw ? (raw.index ? null : bookshelf.import(raw).as(url)) : bookshelf.as(url)),
+        ] = React.useState(raw ? (raw.index ? raw : bookshelf.import(raw).as(url)) : bookshelf.as(url)),
         $go = React.useCallback((url, data) => {
             history.replaceState(null, null, url);
             setTarget(data);
         }, []);
 
     try {
-        if (!target) {
-            const matched = url.match(/^(.*\/)([^\/]+)\/(\d+)$/);
-            if (!matched) throw '无法找到正确的功能路径。';
-            raw.index = + matched[3];
-            bookshelf.as(matched[1]).fetch(matched[2]).then(novel => setTarget(novel.import(raw) as any));
-            return <address>Loading...</address>;
-        }
-        if ('author' in target) return <view.TOC data={ target } $go={ $go } />;
-        if ('prefix' in target) return <view.Bookshelf data={ target } $go={ $go } />;
-        if ('index' in target) return <view.Chapter data={ target } $go={ $go } />;
-        throw target;
+        if ('author' in target) return (
+            <view.TOC data={ target } $go={ $go } />
+        );
+        if ('prefix' in target) return (
+            <view.Bookshelf data={ target } $go={ $go } />
+        );
+        return (
+            <view.Chapter data={ target } $go={ $go } />
+        );
     } catch (error) {
         return (
             <view.Error>{ error }</view.Error>
--- share/gui/js/react/view/chapter/index.tsx
+++ share/gui/js/react/view/chapter/index.tsx
@@ -1,16 +1,26 @@
 import * as React from 'react';
 
+import Mobile from './mobile';
+import Desktop from './desktop';
+
 export default function (props: { [prop: string]: any }) {
-    const chapter = props.data.read();
+    // const chapter = props.data.read();
+    const [
+        mobile,
+        setMobile
+    ] = React.useState(window.innerWidth < 1025 && window.innerWidth < window.innerHeight);
+    React.useEffect(() => {
+        const fn = () => setMobile(window.innerWidth < 1025 && window.innerWidth < window.innerHeight);
+        window.addEventListener('resize', fn);
+        return () => window.removeEventListener('resize', fn);
+    });
 
     document.body.className = 'page-chapter';
 
+    if (mobile) return (
+        <Mobile data={ props.data } />
+    );
     return (
-        <>
-            <h2>{ chapter.title }</h2>
-            { chapter.paragraphs.map((text: string, index: number) => (
-                <p key={ index }>{ text }</p>
-            )) }
-        </>
-    )
+        <Desktop data={ props.data } />
+    );
 }
